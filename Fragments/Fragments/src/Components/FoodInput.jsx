134a521db8219c7b34e6cbc7a81cf3129e670291
keerthana <keerthanab610@gmail.com>
2025-06-22 07:36:14 +0530

    Events Handling onClick and onChange

--- Fragments/Fragments/src/Components/FoodInput.jsx
+++ Fragments/Fragments/src/Components/FoodInput.jsx
@@ -1,9 +1,13 @@
 import styles from './FoodInput.module.css'
 
 function FoodInput(){
+const HandleOnChange = (event) => {
+    console.log(event.target.value)
+}
+
     return (
         <>
-            <input type="text"  placeholder='Add Item' className={styles['input']}/>
+            <input type="text"  placeholder='Add Item' className={styles['input']} onChange={HandleOnChange}/>
         </>
     )
 }
